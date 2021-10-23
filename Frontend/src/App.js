d3a096d37af0e1a1d2e64c1e6a3b611f45ffec71
ShrutiSharma01 <shruti.jpr02@gmail.com>
2021-10-23 14:04:35 +0530

    Added particle background

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+// import logo from "./logo.svg";
 import "./App.css";
+import "./Pages/Flowchart_Editor/Flowchart.css";
+// import Flowchart from './src/Pages/Flowchart_Editor/Flowchart';
+import Flowchart from "./Pages/Flowchart_Editor/Flowchart";
+import ParticleBackground from "./ParticleBackground";
 
 function App() {
-    return <h1>This is the App</h1>;
+    return (
+        <div className = "App">
+            <ParticleBackground />
+            <Flowchart />
+        </div>
+    )
 }
 
 export default App;
---
+++ Frontend/src/Pages/Flowchart_Editor/Flowchart.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Flowchart.css"
+
+function Flowchart() {
+    return (
+        <div className = "flowchart">
+
+        </div>
+    )
+}
+
+export default Flowchart;
+
