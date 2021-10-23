99253d9ecd2e74b2cd7d3792ec9a8481c6d718e4
Ayush <ayush01.dhoot@gmail.com>
2021-10-23 16:29:30 +0530

    login page added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import "./Pages/Flowchart_Editor/Flowchart.css";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
     return (
         <div className="App">
-            <Home />
+            <Login/>
         </div>
     );
 }
